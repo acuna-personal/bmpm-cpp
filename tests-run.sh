c7f549ea9c916118be8e0ff8eeb15cd8101507e0
Ben Kazez <ben@mobiata.com>
2017-08-28 17:40:57 +0100

    Run tests 500/type at a time, randomly chosen

--- tests-run.sh
+++ tests-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "concatenating test suites from dk data..."
-cat tests/suite-ash.txt tests/suite-gen.txt tests/suite-sep.txt > tests/suite-all.txt
+rm tests/suite-all.txt
+gshuf tests/suite-ash.txt | head -500 >> tests/suite-all.txt
+gshuf tests/suite-sep.txt | head -500 >> tests/suite-all.txt
+gshuf tests/suite-gen.txt | head -500 >> tests/suite-all.txt
+#cat tests/suite-ash.txt tests/suite-gen.txt tests/suite-sep.txt > tests/suite-all.txt
 wc -l tests/suite-all.txt
 
 echo "running tests..."
