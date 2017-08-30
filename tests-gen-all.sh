8e8317d254749127b3ee1fbff491313e644f04f5
Ben Kazez <ben@mobiata.com>
2017-08-30 10:51:00 +0200

    Remove old suites before generating new ones to avoid inconsistent suiteness

--- tests-gen-all.sh
+++ tests-gen-all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm tests/suite-gen.txt
+rm tests/suite-ash.txt
+rm tests/suite-sep.txt
+
 php bmver307/tests-gen.php tests/dk.txt tests/suite-gen.txt gen auto
 php bmver307/tests-gen.php tests/dk.txt tests/suite-ash.txt ash auto
 php bmver307/tests-gen.php tests/dk.txt tests/suite-sep.txt sep auto
