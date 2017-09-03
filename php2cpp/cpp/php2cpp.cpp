fd870eba6314a84893e68efc71fe85a4601cd08f
Ben Kazez <ben@mobiata.com>
2017-09-03 10:04:51 +0100

    add strtolower

--- php2cpp/cpp/php2cpp.cpp
+++ php2cpp/cpp/php2cpp.cpp
@@ -1,6 +1,15 @@
 // php funcs
 
-std::string::size_type strlen(std::string str);
+#include <iostream>
+#include <string>
+
+
+std::string::size_type strlen(std::string str)
 {
 	return str.length();
+}
+
+std::string strtolower(std::string str)
+{
+	return std::tolower(c, std::locale("en_US.utf8"));
 }
--- php2cpp/cpp/php2cpp.h
+++ php2cpp/cpp/php2cpp.h
@@ -8,7 +8,37 @@
 // placeholder for unknown types from PHP
 #define php_type void*
 
+php_type array_merge();
+php_type trim();
+php_type debug_print_backtrace();
+php_type in_array();
+php_type preg_match();
+php_type chr();
+php_type strtr();
+php_type preg_replace();
+php_type preg_split();
+php_type count();
 std::string::size_type strlen(std::string str);
+std::string strtolower(std::string str);
+php_type substr();
+php_type printf();
+php_type strpos();
+php_type print_r();
+php_type utf8_encode();
+php_type html_entity_decode();
+php_type mb_strtolower();
+php_type pow();
+php_type log();
+php_type floor();
+php_type mb_detect_encoding();
+php_type str_replace();
+php_type explode();
+php_type implode();
+php_type array_pop();
+php_type ord();
+php_type dechex();
+php_type fopen();
+php_type fgets();
 
 #else
 #endif
